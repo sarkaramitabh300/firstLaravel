780400580f5d112163cdec1cb2a77f83c4927993
Amitabh Sarkar <amitabhs>
2022-07-06 12:15:02 +0530

    added route and views

---
+++ resources/views/post.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Posts</title>
+</head>
+
+<body>
+    Welcome
+    {{-- <h1>{{ $post->title }}</h1>
+    <p>{{ $post->body }}</p>
+    <p>{{ $post->created_at }}</p>
+    <p>{{ $post->updated_at }}</p> --}}
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/',function(){
-    return view('posts',[
-        'posts' => Post::all()
-    ]);
+    return view('post');
 });
 Route::get('/posts/{post}',function($id){
     return view('post',[
